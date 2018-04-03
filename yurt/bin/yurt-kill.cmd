0cf56f67cf6d1cca5ee74db1b54c602a224a753b
tsgouros <tsgouros@gmail.com>
2018-04-03 10:30:12 -0400

    expanded yurt-kill to kill yurt-kill jobs

--- yurt/bin/yurt-kill.cmd
+++ yurt/bin/yurt-kill.cmd
@@ -4,12 +4,12 @@
 export PATH=/bin:/usr/bin
 
 # The kill is done twice to avoid funny timing issues.
-for i in `ps aux | egrep -v -e "^root|^rpc|^dbus|^ntp|^munge|^68|unclutter|^USER" | awk '{print $2}' ` ; do 
+for i in `ps aux | egrep -v -e "^root|^rpc|^dbus|^ntp|^munge|^68|yurt-kill|unclutter|^USER" | awk '{print $2}' ` ; do 
 	kill $i  
 done 
 
 sleep 2
 
-for i in `ps aux | egrep -v -e "^root|^rpc|^dbus|^ntp|^munge|^68|unclutter|^USER" | awk '{print $2}' ` ; do 
+for i in `ps aux | egrep -v -e "^root|^rpc|^dbus|^ntp|^munge|^68|yurt-kill|unclutter|^USER" | awk '{print $2}' ` ; do 
 	kill -KILL $i  
 done 
